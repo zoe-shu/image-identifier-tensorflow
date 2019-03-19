3fdafd16b5587ff50ab60652babbc8776cc1b17e
Zoe <zoe@hello-air.com>
2019-03-19 21:20:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+#Image Identifier with TensorFlow.js#
+```sh
+$ yarn install
+$ yarn watch
+```
+
+#Usage
+1. allow browser to use web camera
+2. show below images to the web camera
+3. the identified result shown on right side
+
+
+
+# This program is based on "TensorFlow.js Example: Transfer Learning to play Pacman via the Webcam"
+# node.js v8.12 required
+
+
+# Update learning model file
+index.js Line:184
+-model.json path
+
+# identified result response to another website/client/server
+ui.js Line:22
+-kiosk's socket.io path
+
+# Re-train Model Instruction:
+Re-train model and download:
+1. comment index.js Line225 "await clickLoad();"
+2. uncomment index.js Line206 "dlModel();"
+3. frontend click the img div to take sample, then click "Train", then click "Play" to download trained model.JSON
+4. uncomment index.js Line225 "await clickLoad();"
+5. comment index.js Line206 "dlModel();"
+
+This example shows you how to predict poses from a webcam using transfer
+learning.
---
+++ index.js
@@ -0,0 +1,229 @@
+
+// import http from 'http';
+// const server = require('http').createServer();
+//
+// const io = new Server();
+// server.listen(3000);
+
+import * as tf from '@tensorflow/tfjs';
+
+import {ControllerDataset} from './controller_dataset';
+import * as ui from './ui';
+import {Webcam} from './webcam';
+// import io from 'socket.io-client';
+
+// const app = express();
+// const server = http.createServer();
+
+// const io_server = io('http://192.168.1.4:8002/');
+// const io_here = io('http://localhost:1234/');
+// io.on('connection', (socket) => {
+//   console.log('1234 user connected');
+//   io_server.emit('hi', 'hi from 1234');
+//   // ...
+// });
+// io_here.on('connection', function(socket){
+//     io_server.emit('hi', 'hi from 1234');
+//     socket.on('hi', function(msg){
+//       io_here.emit('return', 'hi from server');
+//       console.log(msg);
+//     });
+//     console.log('1234 user connected');
+//     // socket.on('scan', function (data) {
+//     //   if(data.status == 'scan'){
+//     //     io.emit('scan-card', data);
+//     //   }else if(data.status == 'finish'){
+//     //     io.emit('scan-finish', data);
+//     //   }
+//
+//       // console.log(msg);
+//     // });
+//
+// });
+
+// The number of classes we want to predict. In this example, we will be
+// predicting 4 classes for up, down, left, and right.
+const NUM_CLASSES = 5;
+// A webcam class that generates Tensors from the images from the webcam.
+const webcam = new Webcam(document.getElementById('webcam'));
+
+// The dataset object where we will store activations.
+const controllerDataset = new ControllerDataset(NUM_CLASSES);
+
+let mobilenet;
+let model;
+
+// Loads mobilenet and returns a model that returns the internal activation
+// we'll use as input to our classifier model.
+
+async function loadMobilenet() {
+  // const mobilenet = await tf.loadModel('./google_model/model.json' );
+  const mobilenet = await tf.loadModel(
+      'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+  // const mobilenet = await tf.loadModel('http://192.168.1.4:1234/model/cardmodel.json');
+  // Return a model that outputs an internal activation.
+  const layer = mobilenet.getLayer('conv_pw_13_relu');
+  return tf.model({inputs: mobilenet.inputs, outputs: layer.output});
+}
+
+// When the UI buttons are pressed, read a frame from the webcam and associate
+// it with the class label given by the button. up, down, left, right are
+// labels 0, 1, 2, 3 respectively.
+ui.setExampleHandler(label => {
+  tf.tidy(() => {
+    const img = webcam.capture();
+    controllerDataset.addExample(mobilenet.predict(img), label);
+
+    // Draw the preview thumbnail.
+    ui.drawThumb(img, label);
+  });
+});
+
+/**
+ * Sets up and trains the classifier.
+ */
+
+async function train() {
+  if (controllerDataset.xs == null) {
+    throw new Error('Add some examples before training!');
+  }
+
+  // Creates a 2-layer fully connected model. By creating a separate model,
+  // rather than adding layers to the mobilenet model, we "freeze" the weights
+  // of the mobilenet model, and only train weights from the new model.
+
+  model = tf.sequential({
+    layers: [
+      // Flattens the input to a vector so we can use it in a dense layer. While
+      // technically a layer, this only performs a reshape (and has no training
+      // parameters).
+      tf.layers.flatten({inputShape: [7, 7, 256]}),
+      // Layer 1
+      tf.layers.dense({
+        units: ui.getDenseUnits(),
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling',
+        useBias: true
+      }),
+      // Layer 2. The number of units of the last layer should correspond
+      // to the number of classes we want to predict.
+      tf.layers.dense({
+        units: NUM_CLASSES,
+        kernelInitializer: 'varianceScaling',
+        useBias: false,
+        activation: 'softmax'
+      })
+    ]
+  });
+
+  // const saveResult = await model.save('downloads://cardmodel');
+
+  // Creates the optimizers which drives training of the model.
+  const optimizer = tf.train.adam(ui.getLearningRate());
+  // We use categoricalCrossentropy which is the loss function we use for
+  // categorical classification which measures the error between our predicted
+  // probability distribution over classes (probability that an input is of each
+  // class), versus the label (100% probability in the true class)>
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
+
+  // We parameterize batch size as a fraction of the entire dataset because the
+  // number of examples that are collected depends on how many examples the user
+  // collects. This allows us to have a flexible batch size.
+  const batchSize =
+      Math.floor(controllerDataset.xs.shape[0] * ui.getBatchSizeFraction());
+  if (!(batchSize > 0)) {
+    throw new Error(
+        `Batch size is 0 or NaN. Please choose a non-zero fraction.`);
+  }
+
+  // Train the model! Model.fit() will shuffle xs & ys so we don't have to.
+  model.fit(controllerDataset.xs, controllerDataset.ys, {
+    batchSize,
+    epochs: ui.getEpochs(),
+    callbacks: {
+      onBatchEnd: async (batch, logs) => {
+        ui.trainStatus('Loss: ' + logs.loss.toFixed(5));
+        await tf.nextFrame();
+      }
+    }
+  });
+}
+
+let isPredicting = false;
+
+async function predict() {
+  ui.isPredicting();
+  while (isPredicting) {
+    const predictedClass = tf.tidy(() => {
+      // Capture the frame from the webcam.
+      const img = webcam.capture();
+
+      // Make a prediction through mobilenet, getting the internal activation of
+      // the mobilenet model.
+      const activation = mobilenet.predict(img);
+
+      // Make a prediction through our newly-trained model using the activation
+      // from mobilenet as input.
+      const predictions = model.predict(activation);
+
+      // Returns the index with the maximum probability. This number corresponds
+      // to the class the model thinks is the most probable given the input.
+      return predictions.as1D().argMax();
+    });
+
+    const classId = (await predictedClass.data())[0];
+    predictedClass.dispose();
+
+    ui.predictClass(classId);
+    await tf.nextFrame();
+  }
+  ui.donePredicting();
+}
+
+async function loadModel(){
+  model = await tf.loadModel('http://localhost:1234/model/cardmodel.json');
+}
+document.getElementById('loadmodel').addEventListener('click', async () => {
+  loadModel();
+  ui.loadStatus('Model Imported');
+});
+
+async function clickLoad(){
+  loadModel();
+  ui.loadStatus('Model Imported');
+  setTimeout(function(){ ui.clickPlay(); }, 5000);
+}
+
+document.getElementById('train').addEventListener('click', async () => {
+  ui.trainStatus('Training...');
+  await tf.nextFrame();
+  await tf.nextFrame();
+  isPredicting = false;
+  train();
+});
+document.getElementById('predict').addEventListener('click', () => {
+  isPredicting = true;
+  // dlModel(); //uncomment for download trained model
+  predict();
+});
+async function dlModel(){
+  await model.save('downloads://cardmodel');
+}
+async function init() {
+  try {
+    await webcam.setup();
+  } catch (e) {
+    document.getElementById('no-webcam').style.display = 'block';
+  }
+  mobilenet = await loadMobilenet();
+
+  // Warm up the model. This uploads weights to the GPU and compiles the WebGL
+  // programs so the first time we collect data from the webcam it will be
+  // quick.
+  tf.tidy(() => mobilenet.predict(webcam.capture()));
+  ui.init();
+  await clickLoad();
+}
+
+// Initialize the application.
+init();
